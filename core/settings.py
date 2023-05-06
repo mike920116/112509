889af5dfba165f022f2284f93ebba14609f4fb13
mike920116 <10856037@ntub.edu.tw>
2023-05-06 17:33:09 +0800

    使用ImageFiled

--- core/settings.py
+++ core/settings.py
@@ -154,7 +154,8 @@ STATIC_URL = 'static/'
 # 2023/5/6
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+# 2023/5/6
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- first/models.py
+++ first/models.py
@@ -9,7 +9,13 @@ class Post(models.Model):
     is_public = models.BooleanField(default=True)
 
     #2023/4/29
-    tags = models.ManyToManyField(to='Tag')
+    # tags = models.ManyToManyField(to='Tag')
+
+    # 2023/5/6
+    tags = models.ManyToManyField(to="Tag", blank=True)
+    image = models.ImageField(upload_to="posts/%Y/%m/%d/", null=True, blank=True)
+    # null -> 可以存放 null 到資料庫中
+    # blank -> 這個欄位是可以不填寫的
 
     # 自動產生的欄位
     # comment_set => 代表了與 Comment 資料表的關聯（<model>_set）
